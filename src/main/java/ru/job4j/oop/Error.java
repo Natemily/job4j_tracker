fcb0c83357875879e676da3f534d834e89a2fa64
Natemily <nerezko.n@gmail.com>
2022-01-25 16:10:34 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public  Error() {
+    }
+
+    public  Error(boolean active, int status, String message) {
+        this.active = active;
+        this.message = message;
+        this.status = status;
+    }
+
+    public void printInfo() {
+        System.out.println("Состояние онлайн: " + active);
+        System.out.println("Статус: " + status + " уровень.");
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error noError = new Error(true, 2, "Все работает");
+        noError.printInfo();
+        Error defError = new Error();
+        defError.printInfo();
+        Error error = new Error(false, 5, "Не работает");
+        error.printInfo();
+    }
+}
