f242f049ea82ad16418ce0cd17120a972f77785d
Natemily <nerezko.n@gmail.com>
2022-02-08 14:19:34 +0300

    4. Сравнение моделей. Метод equals [#174893 #261953]

--- src/main/java/ru/job4j/pojo/Product.java
+++ src/main/java/ru/job4j/pojo/Product.java
@@ -15,7 +15,8 @@ public class Product {
             return false;
         }
         Product product = (Product) o;
-        return count == product.count && Objects.equals(name, product.name);
+        return count == product.count
+                && Objects.equals(name, product.name);
     }
 
     @Override
