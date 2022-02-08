39557cae4820862b4142c3b1ca8d04abebf48cc1
Natemily <nerezko.n@gmail.com>
2022-02-08 17:39:19 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,6 +10,7 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String timeFormat = item.getCreated().format(formatter);
         System.out.println("Текущие дата и время: " + timeFormat);
+        System.out.println(item);
     }
 
 }
