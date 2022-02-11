2b7f31567ea0addd227a1019eff56315f22be672
Natemily <nerezko.n@gmail.com>
2022-02-11 10:11:19 +0300

    3. Интерфейс Input [#181092 #262908]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -2,12 +2,18 @@ package ru.job4j.poly;
 
 public class Bus implements Transport {
 
+    private int flow;
+
     @Override
     public void drive() {
+        if (flow > 0) {
+            System.out.println("Можно ехать");
+        }
     }
 
     @Override
     public void passenger(int quantity) {
+        flow += quantity;
     }
 
     @Override
