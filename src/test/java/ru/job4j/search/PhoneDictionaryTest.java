8fa0464347ab867d5f84cac1c218da4c6cb7d2e8
Natemily <nerezko.n@gmail.com>
2022-03-04 11:51:27 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #271380]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -16,4 +16,15 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
+
+    @Test
+    public void whenNotFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Natalia");
+        ArrayList<Person> expected = new ArrayList<>();
+        assertThat(persons, is(expected));
+    }
 }
