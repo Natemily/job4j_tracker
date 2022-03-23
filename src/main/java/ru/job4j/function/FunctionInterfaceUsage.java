0c355708d09e7e0d701a4aa04cff64272195ca80
Natemily <nerezko.n@gmail.com>
2022-03-23 12:41:56 +0300

    1.2. Функциональный интерфейс [#247497]

---
+++ src/main/java/ru/job4j/function/FunctionInterfaceUsage.java
@@ -0,0 +1,41 @@
+package ru.job4j.function;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.*;
+
+public class FunctionInterfaceUsage {
+    public static void main(String[] args) {
+        List<String> list = List.of("one", "two", "three", "one", "two", "three");
+        Supplier<Set<String>> sup = () -> new HashSet<>(list);
+        BiConsumer<Integer, String> consumer = (s, s1) -> System.out.println(s + s1);
+        Set<String> strings = sup.get();
+        int i = 1;
+        for (String s : strings) {
+            consumer.accept(i++, " is " + s);
+        }
+        Predicate<String> pred = s -> s.isEmpty();
+        System.out.println("Строка пустая: " + pred.test(""));
+        System.out.println("Строка пустая: " + pred.test("test"));
+        BiPredicate<String, Integer> cond = (s, t) -> s.contains(t.toString());
+        System.out.println("Строка содержит подстроку: " + cond.test("Name123", 123));
+        System.out.println("Строка содержит подстроку: " + cond.test("Name", 123));
+        Function<String, Character> func = s -> s.charAt(2);
+        System.out.println("Третий символ в строке: " + func.apply("first"));
+        System.out.println("Третий символ в строке: " + func.apply("second"));
+        BiFunction<String, Integer, String> biFunc = (s, k) -> s.concat(" ").concat(k.toString());
+        System.out.println("Результат работы бифункции: " + biFunc.apply("Name", 123));
+        System.out.println("Результат работы бифункции: " + biFunc.apply("String number", 12345));
+        UnaryOperator<StringBuilder> builder = b -> b.reverse();
+        System.out.println("Строка после реверса: " + builder.apply(new StringBuilder("String for test")));
+        System.out.println("Строка после реверса: " + builder.apply(new StringBuilder("tset rof gnirtS")));
+        BinaryOperator<StringBuilder> builderBi = (b1, b2) -> b1.append(" ").append(b2);
+        System.out.println(
+                "Строка после объединения: " + builderBi.apply(
+                        new StringBuilder("First string"),
+                        new StringBuilder("Second string")
+                )
+        );
+    }
+}
---
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -0,0 +1,33 @@
+package ru.job4j.function;
+
+import java.util.*;
+import java.util.function.*;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biCon = (s, s1) -> map.put(s, s1);
+        List<String> list = List.of("one", "two", "three", "four", "five", "six", "seven");
+        int i = 1;
+        for (String s : list) {
+            biCon.accept(i++, s);
+        }
+
+        BiPredicate<Integer, String> biPred = (s, s1) -> (s % 2 == 0 || s1.length() == 4);
+        for (Integer s : map.keySet()) {
+            if (biPred.test(s, map.get(s))) {
+                System.out.println("key: " + s + " value: " + map.get(s));
+            }
+        }
+
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+
+        Consumer<String> con = (s) -> System.out.println(s);
+        Function<String, String> func = s -> s.toUpperCase();
+        List<String> strings = List.of("one", "two", "three", "one", "two", "three");
+        for (String s : strings) {
+            func.apply(s);
+            con.accept(s.toUpperCase());
+        }
+    }
+}
