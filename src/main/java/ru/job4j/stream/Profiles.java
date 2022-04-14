2072a668a01d40fbc3dcdd48d674c311c26ca598
Natemily <nerezko.n@gmail.com>
2022-04-14 14:20:24 +0300

    2. Список адресов [#110062 #288494]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -9,7 +9,7 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(task -> task.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
